b25b53646c67358a393d08d6feb457f0048104c7
Ruslan Molchanov <ruslan.molchanov@zensoft.io>
2018-06-18 11:40:55 +0000

    Update UpdateScaffoldRequest.kt

--- src/main/kotlin/io/openfuture/api/domain/scaffold/UpdateScaffoldRequest.kt
+++ src/main/kotlin/io/openfuture/api/domain/scaffold/UpdateScaffoldRequest.kt
@@ -2,9 +2,6 @@ package io.openfuture.api.domain.scaffold
 
 import javax.validation.constraints.NotBlank
 
-/**
- * @author Alexey Skadorva
- */
 data class UpdateScaffoldRequest(
         @field:NotBlank var description: String? = null
 )
