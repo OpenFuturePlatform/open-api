0c72b9cd443414cad0983bdc45e4d9294ec29299
Kadach Alexey <alexey.kadach@zensoft.io>
2018-06-22 18:21:17 +0300

    remove params

---
+++ src/main/resources/db/migration/V1_8__remove_template_params.sql
@@ -0,0 +1,7 @@
+DELETE FROM scaffold_template_properties WHERE name = 'company_name';
+DELETE FROM scaffold_template_properties WHERE name = 'company_address';
+DELETE FROM scaffold_template_properties WHERE name = 'customer_name';
+DELETE FROM scaffold_template_properties WHERE name = 'customer_address';
+DELETE FROM scaffold_template_properties WHERE name = 'service_or_goods_type';
+DELETE FROM scaffold_template_properties WHERE name = 'service_or_goods_amour';
+DELETE FROM scaffold_template_properties WHERE name = 'vat_rate';
