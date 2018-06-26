4bf5d105f2ed35e40cd381f4eb06349b22d2b4a4
Alexey Skadorva <alexey.skadorva@zensoft.io>
2018-06-26 16:08:20 +0300

    Revert removed fields in controller config tests

--- src/test/kotlin/io/openfuture/api/config/ControllerTests.kt
+++ src/test/kotlin/io/openfuture/api/config/ControllerTests.kt
@@ -1,14 +1,17 @@
 package io.openfuture.api.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.openfuture.api.config.propety.AuthorizationProperties
 import io.openfuture.api.entity.auth.OpenKey
 import io.openfuture.api.entity.auth.Role
 import io.openfuture.api.entity.auth.User
 import io.openfuture.api.service.OpenKeyService
+import io.openfuture.api.service.UserService
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.context.annotation.Import
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 
@@ -23,7 +26,16 @@ abstract class ControllerTests {
     protected lateinit var objectMapper: ObjectMapper
 
     @MockBean
+    private lateinit var clientRegistrationRepository: ClientRegistrationRepository
+
+    @MockBean
+    private lateinit var userService: UserService
+
+    @MockBean
     protected lateinit var keyService: OpenKeyService
+
+    @MockBean
+    private lateinit var properties: AuthorizationProperties
 
 
     protected fun createOpenKey(roles: Set<Role>): OpenKey {
