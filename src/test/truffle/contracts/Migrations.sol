f418fcaabeefc24bc7dc3025bf37ceec44b7f1a3
igor.pahomov <igor.pahomov@zensoft.io>
2018-06-27 15:07:15 +0300

    OP-44-Scaffold: preparing for docker integration

--- src/test/truffle/contracts/Migrations.sol
+++ src/test/truffle/contracts/Migrations.sol
@@ -1,10 +1,10 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.19;
 
 contract Migrations {
   address public owner;
   uint public last_completed_migration;
 
-  constructor() public {
+  function Migrations() public {
     owner = msg.sender;
   }
 
--- src/test/truffle/truffle.js
+++ src/test/truffle/truffle.js
@@ -15,4 +15,11 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+    networks: {
+        development: {
+            host: process.env.RPC_HOST || "localhost",
+            port: 8545,
+            network_id: "*"
+        }
+    }
 };
