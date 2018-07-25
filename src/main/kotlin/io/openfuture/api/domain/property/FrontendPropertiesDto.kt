046a920da743e79807c0177415920d0c0fbb0932
Alexey <sisipun@gmail.com>
2018-07-25 11:33:26 +0300

    add platform address to frontend properties

--- src/main/kotlin/io/openfuture/api/domain/property/FrontendPropertiesDto.kt
+++ src/main/kotlin/io/openfuture/api/domain/property/FrontendPropertiesDto.kt
@@ -3,5 +3,6 @@ package io.openfuture.api.domain.property
 data class FrontendPropertiesDto(
         val networkAddress: String,
         val networkVersion: String,
-        val openTokenAddress: String
+        val openTokenAddress: String,
+        val platformAddress: String
 )
