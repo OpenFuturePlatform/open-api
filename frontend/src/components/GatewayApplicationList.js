91e3100b40d39ea810d8e6e67a926a8dd3fd698a
Elaman Nazarkulov <physcom93@gmail.com>
2023-10-18 13:07:34 +0600

    Frontend application list remove button style

--- frontend/src/components/GatewayApplicationList.js
+++ frontend/src/components/GatewayApplicationList.js
@@ -4,8 +4,8 @@ import {Button, Card, Divider, Grid, Icon} from 'semantic-ui-react';
 import {ProjectPagination} from '../components-ui/ProjectPagination';
 import {GATEWAY_LIMIT} from '../const';
 import {
-    fetchGatewayApplications,
-    removeGatewayApplication
+  fetchGatewayApplications,
+  removeGatewayApplication
 } from "../actions/gateways";
 import {GatewayApplicationRemove} from "./GatewayApplicationRemove";
 import {WordWrap} from "../components-ui/WordWrap";
@@ -13,71 +13,72 @@ import {Link} from "react-router-dom";
 
 
 class GatewayApplicationList extends Component {
-    componentDidMount() {
-        this.fetchGatewayApplications();
-    }
+  componentDidMount() {
+    this.fetchGatewayApplications();
+  }
 
-    fetchGatewayApplications = (offset = 0, limit = GATEWAY_LIMIT) => {
-        this.props.fetchGatewayApplications(offset, limit);
-    };
+  fetchGatewayApplications = (offset = 0, limit = GATEWAY_LIMIT) => {
+    this.props.fetchGatewayApplications(offset, limit);
+  };
 
-    onRemoveGateway = gateway => {
-        this.props.removeGatewayApplication(gateway.id);
-    }
+  onRemoveGateway = gateway => {
+    this.props.removeGatewayApplication(gateway.id);
+  }
 
-    renderApplications() {
-        const gateways = this.props.gateways;
+  renderApplications() {
+    const gateways = this.props.gateways;
 
-        return gateways.list.map((gateway, index) => {
-            return (
-                <Card fluid key={index}>
-                    <Card.Content>
+    return gateways.list.map((gateway, index) => {
+      return (
+        <Card fluid key={index}>
+          <Card.Content>
 
-                        <Grid.Row>
-                            <Grid.Column width={8}>
-                                <Link to={`/applications/${gateway.id}`}>
-                                    <Card.Header>
-                                        <WordWrap>{gateway.name}</WordWrap>
-                                    </Card.Header>
-                                </Link>
-                                <div className="meta">
-                                    Webhook: {gateway.webHook}
-                                </div>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Card.Content>
-                    <Card.Content extra>
-                        <div className='ui three buttons'>
-
-                            <Button basic color='red'>
-                                <GatewayApplicationRemove gateway={gateway} onSubmit={() => this.onRemoveGateway(gateway)} />
-                            </Button>
-
-                        </div>
-                    </Card.Content>
-                </Card>
-            );
-        });
-    }
-
-    render() {
-        const gateways = this.props.gateways;
-        return (
             <Grid.Row>
-                <Grid.Column width={16}>
-                    {this.renderApplications()}
-                    <ProjectPagination limit={GATEWAY_LIMIT} totalCount={gateways.totalCount} onChange={this.fetchGatewayApplications} />
-                </Grid.Column>
+              <Grid.Column width={8}>
+                <Link to={`/applications/${gateway.id}`}>
+                  <Card.Header>
+                    <WordWrap>{gateway.name}</WordWrap>
+                  </Card.Header>
+                </Link>
+                <div className="meta" style={{display: "inline-flex", width: "100%"}}>
+                  <div className="webhook" style={{width: "100%"}}>
+                  Webhook: {gateway.webHook}
+                  </div>
+                  <div className='ui buttons'>
+                    <Button basic color='red'>
+                      <GatewayApplicationRemove gateway={gateway} onSubmit={() => this.onRemoveGateway(gateway)}/>
+                    </Button>
+                  </div>
+                </div>
+
+              </Grid.Column>
             </Grid.Row>
-        );
-    }
+          </Card.Content>
+        </Card>
+      );
+    });
+  }
+
+  render() {
+    const gateways = this.props.gateways;
+    return (
+      <Grid.Row>
+        <Grid.Column width={16}>
+          {this.renderApplications()}
+          <ProjectPagination limit={GATEWAY_LIMIT} totalCount={gateways.totalCount}
+                             onChange={this.fetchGatewayApplications}/>
+        </Grid.Column>
+      </Grid.Row>
+    );
+  }
 }
-const mapStateToProps = ({ gateways }) => ({ gateways: gateways });
+
+const mapStateToProps = ({gateways}) => ({gateways: gateways});
 
 export default connect(
-    mapStateToProps,
-    {
-        fetchGatewayApplications,
-        removeGatewayApplication
-    }
+  mapStateToProps,
+  {
+    fetchGatewayApplications,
+    removeGatewayApplication
+  }
 )(GatewayApplicationList);
